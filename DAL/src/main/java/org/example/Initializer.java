6a700efd9b6514e619eae30f4cab73ab458632b5
Maria <stepanovamasha70@gmail.com>
2023-03-02 20:57:03 +0200

    add gitignore

--- DAL/src/main/java/org/example/Initializer.java
+++ DAL/src/main/java/org/example/Initializer.java
@@ -1,10 +1,11 @@
 package org.example;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Initializer {
-    Student student = new Student();
+    Student std = new Student();
     ContactInfo contactInfo = new ContactInfo();
     public List<Student> studentsList = new ArrayList<>();
 
@@ -16,7 +17,10 @@ public class Initializer {
 
     private void initModels()
     {
-        addStudent(new Student(1, student.createdData(2023, 01, 12), true, "hard learner",
+       /* addStudent(new Student(1, std.createdData(2023, 01, 12), true, "hard learner",
+                new ContactInfo(1, "Maria", "Stepanova", "Oleksandrivna", contactInfo.birthData(2005,8,4 ), "mariastepanova@kitu.nau.edu.ua", "380998882828"),
+                new History(1, new Course(1, "Java", 12346, 1))));*/
+        addStudent(new Student(std.setId(1), std.createdData(2022,10,4), std.setActive(true), std.setComment("hard learner"),
                 new ContactInfo(1, "Maria", "Stepanova", "Oleksandrivna", contactInfo.birthData(2005,8,4 ), "mariastepanova@kitu.nau.edu.ua", "380998882828"),
                 new History(1, new Course(1, "Java", 12346, 1))));
 
--- DAL/src/main/java/org/example/Student.java
+++ DAL/src/main/java/org/example/Student.java
@@ -43,6 +43,7 @@ public class Student {
     public LocalDate getCreatedDate() {
         return createdDate;
     }
+
     public void setCreatedDate(LocalDate createdDate) {
         this.createdDate = createdDate;
     }
