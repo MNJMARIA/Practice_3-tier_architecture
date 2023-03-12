f083110321f2c92e819c6631d2cd29df64a74d32
Maria <stepanovamasha70@gmail.com>
2023-03-12 19:12:37 +0200

    add extra interfaces to expand IRepository and IService

--- BLL/src/main/java/org/example/IExtraService.java
+++ BLL/src/main/java/org/example/IExtraService.java
@@ -1,6 +1,6 @@
 package org.example;
 
 // Extra service to add more functions for Coach and Students Services
-public interface IExtraService {
+public interface IExtraService<T> extends IService {
     //add additional methods if needed
 }
--- DAL/src/main/java/org/example/Repositories/IExtraRepository.java
+++ DAL/src/main/java/org/example/Repositories/IExtraRepository.java
@@ -2,6 +2,5 @@ package org.example.Repositories;
 
 // Extra repository to add more functions for Coach and Students Repositories
 public interface IExtraRepository<T> extends IRepository{
-
     //add additional methods if needed
 }
